1e8e3eeecc29d90c655d69f94845e27eed909c99
hen <techbabbage@gmail.com>
2017-02-11 14:53:26 +0530

    Graph updated

--- app/src/main/java/com/gagandeep/developer/wellnote/GraphActivity.java
+++ app/src/main/java/com/gagandeep/developer/wellnote/GraphActivity.java
@@ -23,31 +23,31 @@ public class GraphActivity extends AppCompatActivity {
 
         if (graphType.equals("Pulse")){
             LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                    new DataPoint(0, 1),
-                    new DataPoint(1, 5),
-                    new DataPoint(2, 3),
-                    new DataPoint(3, 2),
-                    new DataPoint(4, 6)
+                    new DataPoint(72,81),
+                    new DataPoint(81,70),
+                    new DataPoint(70,75),
+                    new DataPoint(75,80),
+                    new DataPoint(80,78)
             });
             graph.addSeries(series);
         }
         if(graphType.equals("Blood Pressure")){
             LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                    new DataPoint(0, 1),
-                    new DataPoint(1, 5),
-                    new DataPoint(2, 3),
-                    new DataPoint(3, 2),
-                    new DataPoint(4, 6)
+                    new DataPoint(78,91),
+                    new DataPoint(91,80),
+                    new DataPoint(80,78),
+                    new DataPoint(78,80),
+                    new DataPoint(80,84)
             });
             graph.addSeries(series);
         }
         if(graphType.equals("Weight")){
             LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                    new DataPoint(0, 1),
-                    new DataPoint(1, 5),
-                    new DataPoint(2, 3),
-                    new DataPoint(3, 2),
-                    new DataPoint(4, 6)
+                    new DataPoint(0,57),
+                    new DataPoint(57,59),
+                    new DataPoint(59,59),
+                    new DataPoint(59,60),
+                    new DataPoint(60,59)
             });
             graph.addSeries(series);
         }
